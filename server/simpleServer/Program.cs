7fc6c4ecc40005a0433af2cff7a21c28e5676bbd
wuforde <david.forde@gmail.com>
2024-10-06 16:09:44 -0500

    server stuff

---
+++ server/simpleServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/test",() => "hello world");
+        app.Run();
+    }
+}
